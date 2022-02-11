2e7002ce6105dc620e7ec982ba634337d042bd55
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-11 13:39:39 -0500

    create server boilerplate

---
+++ server/routes/catsRoutes.js
@@ -0,0 +1,15 @@
+/////////// catsRoutes.js
+const router = require('express').Router();
+
+module.exports = (db) => {
+  // all routes will go here 
+  router.get('/', (req, res) => {
+    const command = "SELECT * FROM urls";
+    db.query(command)
+      .then(data => {
+        res.json(data.rows);
+      })
+  });
+  
+  return router;
+}
