80ae156e6102d642d3bae686f52c1417193d523f
sonia0409 <sonia.singh0409@gmail.com>
2022-02-13 14:24:15 -0500

    add Application component corresponding to exercies component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Application from './components/Application';
+import Footer from './styles/Footer';
+import Header from './styles/Header';
 
 function App() {
   return (
-    <div>
-      Test Test
+    <div className="App">
+      {/* <div> <Header/> </div> */}
+      <div><Application /> </div>
+      {/* <div> <Footer/> </div>     */}
     </div>
   );
 }
