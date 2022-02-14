e61736822ab110fd196f2131a4fd93565b548ce6
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 17:45:24 -0500

    set the current state and refractor

--- client/src/components/ExerciseList.jsx
+++ client/src/components/ExerciseList.jsx
@@ -1,14 +1,35 @@
 import ExerciseListItem from "./ExerciseListItem";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 
 export default function ExerciseList(props) {
-    const { exercises } = props
-    const listOfExercises = exercises.map(exercise => <ExerciseListItem key={exercise.id} name={exercise.name} gifUrl={exercise.gifUrl} />)
+    const [current, setCurrent] = useState("")
+    const { header, exercises, onClick } = props
+    const listOfExercises = exercises.map(exercise => (
+        <ExerciseListItem
+            key={exercise.id}
+            name={exercise.name}
+            gifUrl={exercise.gifUrl}
+            onClick={() => setCurrent(exercise.name)}
+            show={current === exercise.name}
+        />)
+    )
 
     return (
-        <div>
-            <h2>Exercises</h2>
-            <div>{listOfExercises}</div>
-        </div>
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <Grid container spacing={"auto"}>
+                <Grid item xs={12} md={6}>
+                    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                        {header}
+                    </Typography>
+                    {listOfExercises}
+
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
