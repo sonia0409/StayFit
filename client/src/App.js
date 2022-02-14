ce2f0e317a51c0a3a695e5bef9b75f1d9a95eda5
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 17:42:08 -0500

    define react route paths to render exercises , exercise components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Application from './components/Application';
+import { Routes, Route } from "react-router-dom"
+import Exercises from './pages/Exercises';
+import Exercise from './pages/Exercise';
 
 function App() {
   return (
     <div className="App">
-      {/* <div> <Header/> </div> */}
+      {/*  <div> <Header/> </div> }/:name
       <div><Application /> </div>
-      {/* <div> <Footer/> </div>     */}
+       <div> <Footer/> </div>     } */}
+      <Routes>
+        <Route path="/" element={ <Application/> } />
+        <Route path="exercises/:part" element={ <Exercises/> } />
+        <Route path="exercises/:part/exercise/:name" element={ <Exercise/> } />
+      </Routes>
+
     </div>
   );
 }
