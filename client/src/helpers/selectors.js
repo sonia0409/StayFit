a8b3fb5d478cbe0a6676b815cc70f136fc449b3f
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 09:57:25 -0500

    helper functions to get exercises by body part or the muscel name

---
+++ client/src/helpers/selectors.js
@@ -0,0 +1,17 @@
+//function to filter the exercises based on the BodyPart and the partName
+export const getExerciseByBodyPartName = (exercises, bodyPartName) => {
+  //exercises is an array of objects
+  const exercisesByBodyPart = exercises.filter(
+    (exercise) => exercise.bodyPart === bodyPartName.lowerCase()
+  );
+  //return filtered array of objects
+  return exercisesByBodyPart;
+};
+
+//function to filter the exercise based on the Muscels and the muscelName
+export const getExerciseByMuscelsName = (exercises, muscelName) => {
+    const exercisesByMuscelsName = exercises.filter(
+        (exercise) => exercise.target === muscelName.lowerCase()
+    )
+    return exercisesByMuscelsName;
+};
