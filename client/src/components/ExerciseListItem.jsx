b793a71dd0fc193956d98e0c1903620640ccdce7
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 17:46:57 -0500

    set show to current state

--- client/src/components/ExerciseListItem.jsx
+++ client/src/components/ExerciseListItem.jsx
@@ -1,13 +1,18 @@
 import Item from "./Item";
 
+import ShowExercise from "./ShowExercise";
+
 export default function ExerciseListItem(props) {
-    const { name } = props;
+    const { name, gifUrl, onClick } = props;
 
     return (
-        
-        <div class="btn-group dropend">
-            <Item exerciseItem={name}/>
+
+        <div className="btn-group dropend">
+            <Item exerciseItem={name} onClick={onClick} />
+            {props.show && <ShowExercise name={name} gifUrl={gifUrl} />}
         </div>
-       
+
     );
+
 }
+
