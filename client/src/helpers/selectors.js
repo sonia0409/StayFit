39004bd0b6c650447c54faf3272a34d9ca6ae1c5
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 17:50:17 -0500

    define new function- to route the exercise type

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -1,6 +1,9 @@
 //function to filter the exercises based on the BodyPart and the partName
 export const getExerciseByBodyPartName = (exercises, bodyPartName) => {
   //exercises is an array of objects
+  if (!bodyPartName) {
+    return [];
+  }
   const exercisesByBodyPart = exercises.filter(
     (exercise) => exercise.bodyPart === bodyPartName.toLowerCase()
   );
@@ -9,9 +12,25 @@ export const getExerciseByBodyPartName = (exercises, bodyPartName) => {
 };
 
 //function to filter the exercise based on the Muscels and the muscelName
-export const getExerciseByMusclesName = (exercises, muscelName) => {
-    const exercisesByMuscelsName = exercises.filter(
-        (exercise) => exercise.target === muscelName.toLowerCase()
-    )
-    return exercisesByMuscelsName;
+export const getExerciseByMusclesName = (exercises, muscleName) => {
+  if (!muscleName) {
+    return [];
+  }
+  const exercisesByMusclesName = exercises.filter(
+    (exercise) => exercise.target === muscleName.toLowerCase()
+  );
+  return exercisesByMusclesName;
+};
+
+export const getExercises = (exercises, part, name) => {
+  if (!name) {
+    return [];
+  }
+  if (part === "Muscles") {
+    part = "target";
+  }
+  if (part === "Body Parts") {
+    part = "bodyPart";
+  }
+  return exercises.filter((exercise) => exercise[part] === name.toLowerCase());
 };
