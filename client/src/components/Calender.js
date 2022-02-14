68a57aeb6d40dd37bec92e4b143b7a4dccb3d9c9
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-14 11:40:18 -0500

    remove console logs and commented out code

--- client/src/components/Calender.js
+++ client/src/components/Calender.js
@@ -4,15 +4,12 @@ import WeeklyCalender from './WeeklyCalender';
 
 export default function Calender() {
   const [selectedDate, setSelectedDate] = useState(new Date())
-  console.log("selected day: ", selectedDate)
-  // useEffect(() => {
-  //   setSelectedDate(Date.now())
-  // }, []);
+
   const splitDate = selectedDate.toDateString().split(' ');
-  // console.log(selectedDate)
+
   return (
     <div>
-      Calender page
+      Calender pageeeee
       <h2>Component: Header</h2>
       <h2>Tasks left: 10</h2>
       <h2>{splitDate[1]} {splitDate[2]} </h2>
