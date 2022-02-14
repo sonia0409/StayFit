aa2f135f581f7d732d611992cc1a224b2078fdbd
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-14 01:04:06 -0500

    Create selectedDay state and pass it down as prop to WeeklyCalender.

--- client/src/components/Calender.js
+++ client/src/components/Calender.js
@@ -1,23 +1,24 @@
 import { React, useState } from 'react';
 import DayWorkoutList from '../day_workout_list';
+import WeeklyCalender from './WeeklyCalender';
 
 export default function Calender() {
-  const [selectedDate, setSelectedDate] = useState(new Date().toDateString())
-
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  console.log("selected day: ", selectedDate)
   // useEffect(() => {
   //   setSelectedDate(Date.now())
   // }, []);
-  const splitDate = selectedDate.split(' ');
-  
+  const splitDate = selectedDate.toDateString().split(' ');
+  // console.log(selectedDate)
   return (
     <div>
       Calender page
       <h2>Component: Header</h2>
-      <h2>{splitDate[1]} {splitDate[2]} </h2>
       <h2>Tasks left: 10</h2>
-      <h2>Component: WeekCalender</h2>
-      <DayWorkoutList />
+      <h2>{splitDate[1]} {splitDate[2]} </h2>
+      <WeeklyCalender  currDate={selectedDate} onClick={setSelectedDate} />
 
+      <DayWorkoutList />
       <button className="addNewExercise">
         Add new Exercise
       </button>
