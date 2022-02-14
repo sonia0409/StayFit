8d64286523902b72b84f8c5e695b1d8477303f71
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-14 13:04:23 -0500

    install typescript and eslint. Convert base app.js server to app.ts. Create dist folder to hold js files.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,17 @@
 //  ---------------------------- server/app.js
 // declarations
-require('dotenv').config();
+import 'dotenv/config';
 const { ENVIRONMENT, PORT } = process.env;
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+import express from 'express';
+import morgan from 'morgan';
+
+import bodyParser from 'body-parser';
 
 // db connection
-const db = require('./configs/db.config')
+import db from '../configs/db.config';
 
 // routes import
-const catsRoutes = require('./routes/catsRoutes');
+import catsRoutes from '../routes/catsRoutes';
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use('/cats', catsRoutes(db));
 
 
 app.get('/', (req, res) => {
-	res.json({greetings: 'hello world'});
-})
+  res.json({greetings: 'hello worldddddd'});
+});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
