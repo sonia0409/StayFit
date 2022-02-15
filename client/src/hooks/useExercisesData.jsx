418cd947828dc9ca97d22f081a713f46fcda7ceb
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 20:11:14 -0500

    set api

--- client/src/hooks/useExercisesData.jsx
+++ client/src/hooks/useExercisesData.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
 function useExercisesData() {
     const [exercises, setExercises] = useState([])
-
+    const API_KEY = process.env.REACT_APP_API_KEY
+    
     useEffect(() => {
         const options = {
             method: 'GET',
             url: 'https://exercisedb.p.rapidapi.com/exercises',
             headers: {
                 'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-                'x-rapidapi-key': '45f6331b3amshc902a3bfc3518e1p142db4jsn8d53b2de128d'
+                'x-rapidapi-key': `${API_KEY}`
             }
         };
 
