55d47ffd2011d3f4a01bc207101fc4c475de089c
sonia0409 <sonia.singh0409@gmail.com>
2022-02-14 19:41:43 -0500

    create function to make api call- set the exercises state

--- client/src/hooks/useExercisesData.jsx
+++ client/src/hooks/useExercisesData.jsx
@@ -2,7 +2,28 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function useExercisesData() {
+    const [exercises, setExercises] = useState([])
 
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://exercisedb.p.rapidapi.com/exercises',
+            headers: {
+                'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+                'x-rapidapi-key': '45f6331b3amshc902a3bfc3518e1p142db4jsn8d53b2de128d'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+            setExercises(response.data)
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    }, [])
+
+    return { exercises };
 }
 
 export default useExercisesData;
