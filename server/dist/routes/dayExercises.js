5b7312351534c8773a563589b4bc9da865c55485
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-15 11:21:33 -0500

    refactor get request

--- server/dist/routes/dayExercises.js
+++ server/dist/routes/dayExercises.js
@@ -7,9 +7,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 function default_1(db) {
-    // GET: '/day-exercises'
+    // GET: '/day-exercises/:userid/:date'
+    //  date => 'Mon Feb 14 2022'
     router.get('/:userid/:date', (req, res) => {
-        console.log(req.params);
+        // console.log(req.params);
+        const { date, userid } = req.params;
         const command = `
     SELECT * FROM day_exercises 
     JOIN users ON user_id = users.id
@@ -17,7 +19,24 @@ function default_1(db) {
     WHERE date = $1 
     AND user_id = $2
     `;
-        db.query(command, [req.params.date, req.params.userid])
+        db.query(command, [date, userid])
+            .then((data) => {
+            res.json(data.rows);
+        });
+    });
+    // POST: '/day-exercises/:userid/:date'
+    //  date => 'Mon Feb 14 2022'
+    router.post('/:userid/:date', (req, res) => {
+        // console.log(req.params);
+        const { date, userid } = req.params;
+        const command = `
+    SELECT * FROM day_exercises 
+    JOIN users ON user_id = users.id
+    JOIN exercises on exercise_id = exercises.id 
+    WHERE date = $1 
+    AND user_id = $2
+    `;
+        db.query(command, [date, userid])
             .then(data => {
             res.json(data.rows);
         });
