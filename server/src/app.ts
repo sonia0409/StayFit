2cf67f95cc16fe7b327ea44f72c73ebe770748a3
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-15 12:12:18 -0500

    install cors and user in server for backend requests.

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,12 +13,14 @@ import db from '../configs/db.config';
 // routes import
 import usersRoutes from '../routes/usersRoutes';
 import dayExercisesRoutes from '../routes/dayExercises';
+import cors from 'cors';
 
 const app = express();
 
 // middleware setup
 app.use(morgan(ENVIRONMENT));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/users', usersRoutes(db));
 app.use('/day-exercises', dayExercisesRoutes(db));
