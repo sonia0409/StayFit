e3d2bfe8e11291dc654cfe37f0e915cc12a9adf4
sonia0409 <sonia.singh0409@gmail.com>
2022-02-15 22:43:49 -0500

    add put route to edit the input from form

--- server/routes/dayExercises.ts
+++ server/routes/dayExercises.ts
@@ -37,7 +37,7 @@ export default function (db) {
       .catch((error) => console.log(error.message));
   });
   
-  //Insert and update the day_exercises_list
+  //Add the day_exercises_list
   router.post("/:userid/:date/new", (req, res) => {
     console.log("form values recieved", req.body);
     const { date, userid } = req.params;
@@ -115,6 +115,84 @@ export default function (db) {
         res.status(500).send(error.message);
       });
   });
+
+
+  //edit the form values
+  
+  router.put("/:exercise_id", (req, res) => {
+    const { exercise_id } = req.params;
+    console.log("edited form values recieved", req.body);
+    const {
+      name,
+      weight,
+      duration,
+      sets,
+      reps,
+      mo = false,
+      tu = false,
+      we = false,
+      th = false,
+      fr = false,
+      sa = false,
+      su = false,
+    } = req.body;
+    const exercisesArray = [
+      name,
+      weight,
+      duration,
+      sets,
+      reps,
+      exercise_id
+    ];
+    const exercisesQuery = `
+    UPDATE exercises 
+    SET name = $1, 
+        weight = $4,
+        duration = $5,
+        sets = $6, 
+        reps = $7, 
+        WHERE exercises.id = $8
+    `;
+    const recurringQuery = `
+    UPDATE day_exercises 
+    SET 
+      recurring_monday = $1,
+      recurring_tuesday = $2,
+      recurring_wednesday = $3,
+      recurring_thursday = $4,
+      recurring_friday = $5,
+      recurring_saturday = $6,
+      recurring_sunday = $7, 
+      WHERE exercise_id = $8
+    `;
+
+    db.query(exercisesQuery, exercisesArray)
+      .then((results) => {
+        const recurringArray = [
+          mo,
+          tu,
+          we,
+          th,
+          fr,
+          sa,
+          su,
+          exercise_id
+        ];
+        console.log("recurring array====>", recurringArray);
+
+        return db.query(recurringQuery, recurringArray);
+      })
+      .then((data) => {
+        console.log(data);
+        res.status(200).send("successfully submitted!");
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(error.message);
+      });
+  });
+
+  
 
   return router;
 }
