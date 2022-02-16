1f9ab321011bbfff89c1886c17c6193868f925b6
sonia0409 <sonia.singh0409@gmail.com>
2022-02-15 21:18:35 -0500

    add post for form and patch - to update is_complete status

--- server/routes/dayExercises.ts
+++ server/routes/dayExercises.ts
@@ -1,12 +1,12 @@
 /////////// day_exercisesRoutes.js
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
 export default function (db) {
   // GET: '/day-exercises/:userid/:date'
   //  date => 'Mon Feb 14 2022'
-  router.get('/:userid/:date', (req, res) => {
+  router.get("/:userid/:date", (req, res) => {
     // console.log(req.params);
     const { date, userid } = req.params;
     const command = `
@@ -19,39 +19,59 @@ export default function (db) {
     db.query(command, [date, userid])
       .then((data) => {
         res.json(data.rows);
-      });
+      })
+      .catch((error) => res.status(500).send(error.message));
   });
-
   // POST: '/day-exercises/:userid/:date'
   //  date => 'Mon Feb 14 2022'
-  router.post('/:userid/:date', (req, res) => {
-    // console.log(req.params);
-    const { date, userid } = req.params;
-    const command = `
-    SELECT * FROM day_exercises 
-    JOIN users ON user_id = users.id
-    JOIN exercises on exercise_id = exercises.id 
-    WHERE date = $1 
-    AND user_id = $2
-    `;
-    db.query(command, [date, userid])
-      .then(data => {
-        res.json(data.rows);
-      });
-  });
 
+  //update the is_complete status
+  router.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    const query = ` UPDATE day_exercises
+    SET is_completed = NOT is_completed
+    WHERE day_exercises.id = $1
+    `;
+    db.query(query, [id])
+      .then(() => res.send(200))
+      .catch((error) => console.log(error.message));
+  });
+  
   //Insert and update the day_exercises_list
-  router.post('/new', (req, res) => {
-    const { name, muscleGroup, bodyPart, weight, duration, sets, reps, userid, exercise_id, date, mo, tu, we, th, fr, sa, su } = req.body
-    const exercisesArray = [name, muscleGroup, bodyPart, weight, duration, sets, reps]
-    const recurringArray = [userid, exercise_id, date, mo, tu, we, th, fr, sa, su]
-    const exercisesQuery =
-      `
-      INSERT INTO exercises (name, muscleGroup, bodyPart, weight, duration, sets, reps) 
-      VALUES ($1::text, $2::text, $3::text, $4::integer, $5::integer, $6::integer, $7::integer)
-      `
-    const recurringQuery =
-      `
+  router.post("/:userid/:date/new", (req, res) => {
+    console.log("form values recieved", req.body);
+    const { date, userid } = req.params;
+    const {
+      name,
+      muscleGroup,
+      bodyPart,
+      weight,
+      duration,
+      sets,
+      reps,
+      mo = false,
+      tu = false,
+      we = false,
+      th = false,
+      fr = false,
+      sa = false,
+      su = false,
+    } = req.body;
+    const exercisesArray = [
+      name,
+      muscleGroup,
+      bodyPart,
+      weight,
+      duration,
+      sets,
+      reps,
+    ];
+    const exercisesQuery = `
+    INSERT INTO exercises (name, muscleGroup, bodyPart, weight, duration, sets, reps) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING id
+    `;
+    const recurringQuery = `
     INSERT INTO day_exercises(
       user_id,
       exercise_id,
@@ -63,30 +83,38 @@ export default function (db) {
       recurring_friday,
       recurring_saturday,
       recurring_sunday
-    ) 
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-    ON CONFLICT (exercise_id) DO
-    UPDATE SET 
-    user_id,      
-    date,
-    recurring_monday,
-    recurring_tuesday,
-    recurring_wednesday,
-    recurring_thursday,
-    recurring_friday,
-    recurring_saturday,
-    recurring_sunday
-    `
+      ) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    `;
 
     db.query(exercisesQuery, exercisesArray)
-      .then(
-        db.query(recurringQuery, recurringArray))
-      .then(data => {
-        console.log(data)
-        res.status(200)
-      })
-      .catch(error => console.log(error));
+      .then((results) => {
+        const exercise_id = results.rows[0].id;
+        const recurringArray = [
+          Number(userid),
+          exercise_id,
+          date,
+          mo,
+          tu,
+          we,
+          th,
+          fr,
+          sa,
+          su,
+        ];
+        console.log("recurring array====>", recurringArray);
 
-  })
+        return db.query(recurringQuery, recurringArray);
+      })
+      .then((data) => {
+        console.log(data);
+        res.status(200).send("successfully submitted!");
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(error.message);
+      });
+  });
+
   return router;
 }
