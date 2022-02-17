4d6b09ffd1bff7c759618838fab57a13c00e5f8f
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-02-16 23:05:30 -0600

    wrap checkboxes in grid

--- client/src/global-components/AddForm.jsx
+++ client/src/global-components/AddForm.jsx
@@ -62,70 +62,88 @@ const AddForm = (props) => {
           <hr />
           <h2>{date}</h2>
         </div>
-        <label className="form-label">Body Part</label>
 
+        {/* Dropdown */}
         <div className="form-dropdown">
-          <Controller
-            name="bodyPart"
-            control={control}
-            render={({ field }) => (
-              <ReactSelect
-                isClearable
-                {...field}
-                options={[
-                  { value: "chest", label: "Chest" },
-                  { value: "lower arms", label: "Lower arms" },
-                  { value: "lower legs", label: "Lower legs" },
-                  { value: "neck", label: "Neck" },
-                  { value: "shoulders", label: "Shoulders" },
-                  { value: "upper arms", label: "Upper arms" },
-                  { value: "upper legs", label: "Upper legs" },
-                  { value: "waist", label: "Waist" },
-                  { value: "back", label: "Back" },
-                  { value: "cardio", label: "Cardio" },
-                  { value: "other", label: "Other" },
-                ]}
+          <Grid container spacing={0}>
+            <Grid item xs={3}>
+              <label className="form-label">Body Part</label>
+            </Grid>
+            <Grid item xs={9}>
+              <Controller
+                name="bodyPart"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    isClearable
+                    {...field}
+                    options={[
+                      { value: "chest", label: "Chest" },
+                      { value: "lower arms", label: "Lower arms" },
+                      { value: "lower legs", label: "Lower legs" },
+                      { value: "neck", label: "Neck" },
+                      { value: "shoulders", label: "Shoulders" },
+                      { value: "upper arms", label: "Upper arms" },
+                      { value: "upper legs", label: "Upper legs" },
+                      { value: "waist", label: "Waist" },
+                      { value: "back", label: "Back" },
+                      { value: "cardio", label: "Cardio" },
+                      { value: "other", label: "Other" },
+                    ]}
+                  />
+                )}
               />
-            )}
-          />
-
-          <label className="form-label">Muscle Group</label>
-          <Controller
-            name="muscleGroup"
-            control={control}
-            render={({ field }) => (
-              <ReactSelect
-                isClearable
-                {...field}
-                options={[
-                  { value: "abductors", label: "Abductors" },
-                  { value: "abs", label: "Abs" },
-                  { value: "biceps", label: "Biceps" },
-                  { value: "calves", label: "Calves" },
-                  {
-                    value: "cardiovascular system",
-                    label: "Cardiovascular System",
-                  },
-                  { value: "delts", label: "Delts" },
-                  { value: "forearms", label: "Forearms" },
-                  { value: "glutes", label: "Glutes" },
-                  { value: "hamstrings", label: "Hamstrings" },
-                  { value: "lats", label: "Lats" },
-                  { value: "levator scapulae", label: "Levator Scapulae" },
-                  { value: "pectorals", label: "Pectorals" },
-                  { value: "quads", label: "Quads" },
-                  { value: "serratus anterior", label: "Serratus Anterior" },
-                  { value: "spine", label: "Spine" },
-                  { value: "traps", label: "Traps" },
-                  { value: "triceps", label: "Triceps" },
-                  { value: "upper back", label: "Upper back" },
-                  { value: "other", label: "other" },
-                ]}
+            </Grid>
+          </Grid>
+        </div>
+        <div className="form-dropdown">
+          <Grid container spacing={0}>
+            <Grid item xs={3}>
+              <label className="form-label">Muscle Group</label>
+            </Grid>
+            <Grid item xs={9}>
+              <Controller
+                name="muscleGroup"
+                control={control}
+                render={({ field }) => (
+                  <ReactSelect
+                    isClearable
+                    {...field}
+                    options={[
+                      { value: "abductors", label: "Abductors" },
+                      { value: "abs", label: "Abs" },
+                      { value: "biceps", label: "Biceps" },
+                      { value: "calves", label: "Calves" },
+                      {
+                        value: "cardiovascular system",
+                        label: "Cardiovascular System",
+                      },
+                      { value: "delts", label: "Delts" },
+                      { value: "forearms", label: "Forearms" },
+                      { value: "glutes", label: "Glutes" },
+                      { value: "hamstrings", label: "Hamstrings" },
+                      { value: "lats", label: "Lats" },
+                      { value: "levator scapulae", label: "Levator Scapulae" },
+                      { value: "pectorals", label: "Pectorals" },
+                      { value: "quads", label: "Quads" },
+                      {
+                        value: "serratus anterior",
+                        label: "Serratus Anterior",
+                      },
+                      { value: "spine", label: "Spine" },
+                      { value: "traps", label: "Traps" },
+                      { value: "triceps", label: "Triceps" },
+                      { value: "upper back", label: "Upper back" },
+                      { value: "other", label: "other" },
+                    ]}
+                  />
+                )}
               />
-            )}
-          />
+            </Grid>
+          </Grid>
         </div>
         {/* Inputs field */}
+        {errors.exerciseName && <p>Exercise Name is required field</p>}
         <Grid container spacing={0}>
           <Grid item xs={3}>
             <label className="form-label"> Exerciese Name</label>
@@ -137,7 +155,6 @@ const AddForm = (props) => {
             />
           </Grid>
         </Grid>
-        {errors.exerciseName && <p>Exercise Name is required field</p>}
         <Grid container spacing={0}>
           <Grid item xs={3}>
             <label className="form-label"> Duration </label>
@@ -162,64 +179,74 @@ const AddForm = (props) => {
             <input {...register("reps")} placeholder="Reps" />
           </Grid>
         </Grid>
-
-        <label className="form-label"> Weight </label>
-
-        <input {...register("weight")} placeholder="Weight" />
-
+        <Grid container spacing={0}>
+          <Grid item xs={3}>
+            <label className="form-label"> Weight </label>
+          </Grid>
+          <Grid item xs={9}>
+            <input {...register("weight")} placeholder="Weight" />
+          </Grid>
+        </Grid>
+        {/* Hiden field for date */}
         <input
           {...register("date")}
           placeholder="Date"
           type="hidden"
           value={date}
         />
-
-        <label className="form-label">Recurring :</label>
-        <div className="form-checkboxes">
-          <label> Mo </label>
-          {/* control your input into the hook by invoking the "field" function */}
-          <Controller
-            name="Mo"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> Tu </label>
-          <Controller
-            name="Tu"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> We </label>
-          <Controller
-            name="We"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> Th </label>
-          <Controller
-            name="Th"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> Fr </label>
-          <Controller
-            name="Fr"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> Sa </label>
-          <Controller
-            name="Sa"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-          <label> Su </label>
-          <Controller
-            name="Su"
-            control={control}
-            render={({ field }) => <Checkbox {...field} />}
-          />
-        </div>
+        {/* Checkboxes  */}
+        <Grid container spacing={0}>
+          <Grid item xs={3}>
+            <label className="form-label">Recurring :</label>
+          </Grid>
+          <Grid item xs={9}>
+            <div className="form-checkboxes">
+              <label> Mo </label>
+              {/* control your input into the hook by invoking the "field" function */}
+              <Controller
+                name="Mo"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> Tu </label>
+              <Controller
+                name="Tu"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> We </label>
+              <Controller
+                name="We"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> Th </label>
+              <Controller
+                name="Th"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> Fr </label>
+              <Controller
+                name="Fr"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> Sa </label>
+              <Controller
+                name="Sa"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+              <label> Su </label>
+              <Controller
+                name="Su"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              />
+            </div>
+          </Grid>
+        </Grid>
         <input type="submit" />
       </form>
     </main>
