16b0b2f03c404ccb7f42bda88ea87915e15f24e4
nikhil tallapureddy <nikhiltallapureddy@gmail.com>
2022-02-21 12:51:49 -0500

    Change stylings

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -7,25 +7,25 @@ export default function AuthProvider(props) {
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
 
-  const login = function(email, password) {
-    const email1 = '123@gmail.com';
-    const id = 1;
-    const name = 'Nik';
-    setUser({ email1, id, name });
-    setAuth(true);
+  const login = async function(email, password) {
+    // const email1 = '123@gmail.com';
+    // const id = 1;
+    // const name = 'Nik';
+    // setUser({ email1, id, name });
+    // setAuth(true);
 
-    // await axios.get(`http://localhost:8080/users/${email}`)
-    //   .then((response) => {
-    //     const userDetails = response.data[0];
-    //     const {id, email } = userDetails;
-    //     const name = userDetails.firstname;
+    await axios.get(`http://localhost:8080/users/${email}`)
+      .then((response) => {
+        const userDetails = response.data[0];
+        const {id, email } = userDetails;
+        const name = userDetails.firstname;
 
-    //     if(password === userDetails.password) {
-    //       setUser({ email, id, name });
-    //       setAuth(true);
-    //     } 
-    //   })
-    //   .catch(e => console.log(e))
+        if(password === userDetails.password) {
+          setUser({ email, id, name });
+          setAuth(true);
+        } 
+      })
+      .catch(e => console.log(e))
   };
 
   const logout = function() {
