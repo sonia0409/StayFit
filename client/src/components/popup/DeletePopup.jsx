52f23b6bec649df1328e5d2fc07398739f03b4a4
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-02-20 22:15:30 -0600

    styling for delete buttons on popup

--- client/src/components/popup/DeletePopup.jsx
+++ client/src/components/popup/DeletePopup.jsx
@@ -2,22 +2,33 @@ import React from "react";
 
 export default function DeletePopup(props) {
   const { handleClose, onSingleDelete, onAllDelete, isRecurring } = props;
-  
+
   return (
     <div className="popup-box">
       <div className="box">
-        <span className="close-icon" onClick={handleClose}>x</span>
-        <div>
-          Confirm deletion:
+        <span className="close-icon" onClick={handleClose}>
+          x
+        </span>
+        <div className="confirm-deletion">
+          <h4>Confirm deletion:</h4>
+          {/* <h4>Delete for:</h4> */}
         </div>
-        <button
-          onClick={() => onSingleDelete()}
-        >Delete exercise for today</button>
-        { isRecurring && 
-          <button
-          onClick={() => onAllDelete()}
-          >Delete for all future dates</button>
-        }
+        <div className="container-delete-buttons">
+          <div>
+            <button id="single-delete" onClick={() => onSingleDelete()}>
+              Delete exercise for today
+              {/* Today only */}
+            </button>
+          </div>
+          <div>
+            {isRecurring && (
+              <button id="all-delete" onClick={() => onAllDelete()}>
+                Delete for all future dates
+                {/* All future dates */}
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
