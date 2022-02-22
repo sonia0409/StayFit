973fc3faadf4eeea8ded49ed8ce1187e51e4ef90
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-02-22 11:41:50 -0600

    add logo-image and logo-text

--- client/src/global-components/Header.jsx
+++ client/src/global-components/Header.jsx
@@ -1,26 +1,26 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import Avatar from '@mui/material/Avatar';
-import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { authContext } from '../providers/AuthProvider';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { authContext } from "../providers/AuthProvider";
 
 export default function MenuAppBar() {
   const { auth, user, logout } = useContext(authContext);
 
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleHomeButton = () => {
-    console.log("Home button clicked!!")
-    navigate("..")
-  }
+    console.log("Home button clicked!!");
+    navigate("..");
+  };
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -28,12 +28,12 @@ export default function MenuAppBar() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-//material ui took inline styling only- figure out the ways to implement differently
+  //material ui took inline styling only- figure out the ways to implement differently
   return (
-    <Box  sx={{ flexGrow: 1 }}>
-      <AppBar style={{ background: '#2c2e43' }} position="static">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar style={{ background: "#2c2e43" }} position="static">
         <Toolbar>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -48,33 +48,39 @@ export default function MenuAppBar() {
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             StayFit
+            {/* <img
+              style={{ background: "red" }}
+              // id="head-image"
+              src="../stay-fit.png"
+            ></img> */}
           </Typography>
           {auth && (
             <div>
-
               <IconButton onClick={handleMenu} sx={{ p: 0 }}>
                 <Avatar
                   alt="Tired man"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxSNIFSen-ODMRSzmOFJkfqBxejIsxvl2_6g&usqp=CAU"
+                  // src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxSNIFSen-ODMRSzmOFJkfqBxejIsxvl2_6g&usqp=CAU"
+                  src="../fitnes-logo.png"
                 />
-
               </IconButton>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem divider={true} disabled={true}>User: { user.name }</MenuItem>
+                <MenuItem divider={true} disabled={true}>
+                  User: {user.name}
+                </MenuItem>
                 {/* <MenuItem divider={true} onClick={handleClose}>My account</MenuItem> */}
                 <MenuItem onClick={logout}>Logout</MenuItem>
               </Menu>
