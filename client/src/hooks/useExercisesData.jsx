a86cc0a11572355641858c9aabfb6e73fc1fa286
sonia0409 <sonia.singh0409@gmail.com>
2022-02-25 17:11:21 -0500

    fix useEffect variables

--- client/src/hooks/useExercisesData.jsx
+++ client/src/hooks/useExercisesData.jsx
@@ -5,16 +5,16 @@ import axios from "axios";
 function useExercisesData() {
     const [exercises, setExercises] = useState([])
     const API_KEY = process.env.REACT_APP_API_KEY
-    const options = {
-        method: 'GET',
-        url: 'https://exercisedb.p.rapidapi.com/exercises',
-        headers: {
-            'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-            'x-rapidapi-key': `${API_KEY}`
-        }
-    };
 
     useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://exercisedb.p.rapidapi.com/exercises',
+            headers: {
+                'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+                'x-rapidapi-key': `${API_KEY}`
+            }
+        };
         axios
             .request(options)
             .then((res) => {
--- client/src/hooks/useWeather.jsx
+++ client/src/hooks/useWeather.jsx
@@ -5,27 +5,27 @@ import axios from "axios";
 function useWeather() {
 
     const [todayWeather, setTodayWeather] = useState({})
-
     const API_KEY = process.env.REACT_APP_API_KEY_WEATHER
-    const options = {
-        method: 'GET',
-        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-        params: {
-            q: 'toronto',
-            lat: 'toronto',
-            lon: 'toronto',
-            id: '2172797',
-            lang: 'null',
-            units: 'metric',
-            mode: 'JSON'
-        },
-        headers: {
-            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-            'x-rapidapi-key': `${API_KEY}`
-        }
-    };
-
+    
     useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+            params: {
+                q: 'toronto',
+                lat: 'toronto',
+                lon: 'toronto',
+                id: '2172797',
+                lang: 'null',
+                units: 'metric',
+                mode: 'JSON'
+            },
+            headers: {
+                'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+                'x-rapidapi-key': `${API_KEY}`
+            }
+        };
+    
         axios
             .request(options)
             .then((res) => {
